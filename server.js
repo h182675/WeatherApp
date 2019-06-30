4bc64c05a39025217693e720105c667bdbd215ae
Daniel pettersen <d.pettersen@gmail.com>
2019-06-30 20:11:52 +0200

    Render view page on / url

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 app.set('view engine','ejs');
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
