f99be77dfead58bbf796b15484858ea192d9284a
Daniel pettersen <d.pettersen@gmail.com>
2019-06-30 20:59:12 +0200

    Added a stylesheet template

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
 const port = 3000;
 
 app.get('/', (req, res) => res.render('index'));
+
+app.post('/', (req,res)=>{
+   let city = req.body.city;
+   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+   res.render('index');
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
