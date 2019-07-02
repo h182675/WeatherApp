705cdb31a29a7ee5bcff971061d512d4f2bb8ebe
Daniel pettersen <d.pettersen@gmail.com>
2019-07-02 14:55:30 +0200

    Created an external json file for storing API key to prevent pushing it to a public repo

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fetch = require("node-fetch");
+const key = require('./ApiKey.json');
 const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 const port = 3000;
 
 app.get('/', (req, res) => res.render('index'));
+const apiKey = key.ApiKey;
+app.post('/', (req, res) => {
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-app.post('/', (req,res)=>{
-   let city = req.body.city;
-   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-
-   res.render('index');
+    fetch(url).then(response => {
+        if (response.status !== 200) {
+            res.render('index', {weather: null, error: 'Error, please try again'});
+        } else {
+            return response.json();
+        }
+    }).then(json=>{
+        let weatherText = `It's ${json.main.temp} degrees in ${json.name}.`;
+        res.render('index', {weather: weatherText, error: null});
+    });
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}...`));
